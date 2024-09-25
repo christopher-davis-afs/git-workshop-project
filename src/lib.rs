f2a05c3f37334975bddef4a880c1e7930c4a8072
Christopher Davis <150722105+christopher-davis-afs@users.noreply.github.com>
2024-09-25 09:17:48 -0400

    lib: Add sum command

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,15 @@ pub fn add<T: Copy + Add<Output = T>>(lhs: T, rhs: T) -> T {
     lhs + rhs
 }
 
+pub fn sum<T: Copy + Add<Output = T>>(summands: &[T]) -> T {
+    let mut sum = summands[0];
+    for (_, summand) in summands.iter().enumerate().filter(|(i, _)| *i != 0) {
+        sum = add(sum, summand.to_owned());
+    }
+
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,5 +23,12 @@ mod tests {
         let num_to_add = 1;
 
         assert_eq!(add(first_num, num_to_add), 3);
+    }
+
+    #[test]
+    fn test_sum() {
+        let nums = [1, 2, 3, 4];
+
+        assert_eq!(sum(&nums), 10);
     }
 }
