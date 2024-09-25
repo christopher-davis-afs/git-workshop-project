b1dbca882c520144e57a42fe74ceca6d9c54364b
Christopher Davis <150722105+christopher-davis-afs@users.noreply.github.com>
2024-09-25 09:36:50 -0400

    lib: Add second add test

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,14 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_add_two_plus_two_equals_four() {
+        let lhs = 2;
+        let rhs = 2;
+
+        assert_eq!(add(lhs, rhs), 4);
+    }
+
+    #[test]
     fn test_add() {
         let first_num = 2;
         let num_to_add = 1;
